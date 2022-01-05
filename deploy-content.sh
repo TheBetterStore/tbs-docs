3bc44addab6e226ddf306b385e0cd17e12625003
Bryce.Cummock <bryce.cummock@servian.com>
2022-01-06 09:38:34 +1300

    Amend deployment

--- deploy-content.sh
+++ deploy-content.sh
@@ -4,4 +4,4 @@ export ENVIRONMENT=prod
 npm run build
 
 aws s3 sync ./build s3://docs-us-east-1-$AWS_ACCOUNT_ID-tbs-$ENVIRONMENT --profile thebetterstore
-aws cloudfront list-distributions --output json --query "DistributionList.Items[?Comment=='docs.thebetterstore.net'].Id" | awk -F '"' '{print $2}' | xargs -I{} aws cloudfront create-invalidation --distribution-id {} --paths "/*"
+aws cloudfront list-distributions --profile thebetterstore --output json --query "DistributionList.Items[?Comment=='docs.thebetterstore.net'].Id" | awk -F '"' '{print $2}' | xargs -I{} aws cloudfront create-invalidation --distribution-id {} --paths "/*" --profile thebetterstore
